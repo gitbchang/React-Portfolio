104a9204f8635157a7d80888de7f3f3730e3a168
Brian Chang <brian.h.chang1@gmail.com>
2017-05-19 12:44:41 -0500

    spotify single page complete

--- src/components/presentation/SpotifyApiProject.jsx
+++ src/components/presentation/SpotifyApiProject.jsx
@@ -29,8 +29,17 @@ class SpotifyApiProject extends Component {
       What Does It Do
     </div>
     <p className="f5 lh-copy measure">
-      Using the Search Bar, it will query for the artist's listening statistics and popular tracks. You can then favorite these tracks, which is stored inside the MongoDB and later shown in the Favorites section. <br />
-      It will also show you statistics based on your favorite 
+      Using the Search Bar, it will query for the artist's listening statistics and popular tracks. You can then favorite these tracks, which is stored inside the MongoDB and later shown in the Favorites section. <br /><br />
+      Login with your Spotify account to see average statistics based on your Top 10 Tracks. 
+    </p>
+      <div className="f3 fw6 mid-gray lh-title mb3">
+      Challenges
+    </div>
+    <p className="f5 lh-copy measure">
+      The first challenge was deciding on a CSS library/framework to use. I decided to use Tachyons for the styling. I discovered that I prefer to use class name inline styling rather than import individual react components,  ex. <span className="code bg-black-70 washed-green">import {`{ Button }`} from 'react-bootstrap'; </span>
+      <br /><br />
+      Deploying My App has not been the smoothest due to Heroku's mLab MongoDB. Will need to debug further. I had to use Robomongo to connect to mLab's server to see if my models were created properly. <br /><br />
+      I am using Axios, a promise based http client, to make requests to my own DB and Spotify's DB. I had to do some debugging due to chaining multiple promise calls together. Also the target of <span className="code bg-black-70 washed-green">'this'</span> will change when inside the <span className="code bg-black-70 washed-green">'promise.then(...'</span> code block.
     </p>
   </div>
 </article>
