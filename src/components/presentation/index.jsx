63fbe7f1c9e1d3cce2f557949f2d7c46066744ee
Brian Chang <brian.h.chang1@gmail.com>
2017-05-19 13:01:36 -0500

    removed extra import

--- src/components/presentation/index.jsx
+++ src/components/presentation/index.jsx
@@ -1,11 +1,9 @@
 import TopHeader from './TopHeader';
 import BottomFooter from './BottomFooter';
 import SpotifyApiProject from './SpotifyApiProject';
-import Resume from './Resume';
 
 export {
   TopHeader,
   BottomFooter,
-  SpotifyApiProject,
-  Resume
+  SpotifyApiProject
 }
